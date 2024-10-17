85e3c67cb0f971cea3e0912c9f2474496e250752
Xueyuan Jiang <alexxyjiang@gmail.com>
2024-10-17 16:23:51 +0800

    cross-platform building by cross-compiling

---
+++ docker/dev/.crossplatform.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <alpine_version> <current_platform> <target_platforms>"
+  echo "e.g. $0 3.20 x86_64 x86_64,aarch64"
+  exit 1
+fi
+
+ALPINE_VERSION="${1}"
+CURRENT_PLATFORM="${2}"
+TARGET_PLATFORMS="${3}"
+if [ ! -d aports ]; then
+  # git clone --depth 1 --branch "${ALPINE_VERSION}-stable" https://github.com/alpinelinux/aports
+  git clone --depth 1 https://github.com/alpinelinux/aports
+fi
+for platform in $(echo ${TARGET_PLATFORMS} | sed 's/,/ /g'); do
+  if [ "${platform}" != "${CURRENT_PLATFORM}" ]; then
+    echo "==== Cross platform support for ${platform} building ===="
+    ./aports/scripts/bootstrap.sh ${platform}
+    echo "==== Cross platform support for ${platform} done ===="
+  fi
+done
